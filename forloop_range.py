ba3c22547fe9c72b1c11379ad29cbb259372c443
Emmanuel Egomson <fegomson@gmail.com>
2022-01-14 11:59:10 -0600

    For loop and Range

---
+++ forloop_range.py
@@ -0,0 +1,29 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+# For loop
+# for i in nums:
+#     print("i =", i)
+
+# Range
+# Starting at 1 and ending at 5
+# range(start, end)
+# my_range = range(2, 5)
+# for i in my_range:
+#     print(i)
+
+# Test if a number is odd or even
+# Print out all the odd numbers from 1 - 20
+# nums = range(1, 21)
+#
+# for i in nums:
+#     if(i % 2 == 0):
+#         print("{} is even".format(i))
+#     else:
+#         print("{} is odd".format(i))
+
+# Round to 2 decimals
+your_float = input("Enter a float: ")
+your_float = float(your_float)
+rounded = print("Rounded to 2 Decimals: {:.2f}".format(your_float))
+
+
